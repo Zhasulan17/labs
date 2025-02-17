c72a6681d8ebdd2babb07b1a99339d753713a180
Zhasulan17 <zhasulanx007@gmail.com>
2025-02-18 01:31:21 +0600

    math

---
+++ lab4/math.py
@@ -0,0 +1,43 @@
+# 1
+import math
+a = int ( input ())
+s = math.pi
+print ( a * s / 180 )
+
+
+#2
+a = int(input()) 
+b = int(input())
+v = int(input())
+j = ( ( 1 / 2 * ( b + v ) * a) )
+print (j)
+
+
+# 3
+import math
+sides = int (input())
+lenth = int (input())
+x = int((sides * (lenth ** 2))/4) * (1/math.tan((math.pi/sides)))
+print (int(x))
+
+
+# 4
+a = int ( input() )
+b = float ( input() )
+print(a*b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
