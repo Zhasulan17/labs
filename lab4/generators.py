4e97f189eb55ec2af83769948dc8c1a9204bcfd4
Zhasulan17 <zhasulanx007@gmail.com>
2025-02-18 01:29:13 +0600

    generators

---
+++ lab4/generators.py
@@ -0,0 +1,54 @@
+# 1
+n = int( input() )
+for i in range(1, n+1):
+    print(i**2)
+
+
+# 2
+n = int( input() )
+for i in range(0, n+1, 2):
+    print (i)
+
+
+# 3
+n = int( input() )
+for i in range(0, n+1):
+    if (i % 3 == 0) and (i % 4 == 0):
+        print (i)
+
+
+# 4
+a = int ( input () )
+b = int ( input () )
+for i in range ( a, b+1 ):
+    print (i ** 2)
+
+
+# 5
+n = int( input() )
+for i in range(n, -1, -1):
+    print (i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
